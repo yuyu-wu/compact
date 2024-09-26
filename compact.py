658be82fb49873133f1e9b03a32b955b378dd1b9
yuyu <swharriet2012@gmail.com>
2024-09-26 11:05:24 +1000

    initial

---
+++ compact.py
@@ -0,0 +1,7 @@
+# compact
+# Write a function called compact. This function accepts a list and returns a list of values that are truthy values, without any of the falsey values.
+
+# compact([0,1,2,"",[], False, {}, None, "All done"])     # [1,2, "All done"]
+
+def compact(lst):
+    return [val for val in lst if val]
